bcaed564a27e1bc1e60ae8f3e131feb6bea0595d
Anjali <anjalikuku098@gmail.com>
2022-02-15 22:11:18 +0530

    Redux and Redux Toolkit

--- Practicing Redux/src/store/index.js
+++ Practicing Redux/src/store/index.js
@@ -1,19 +1,47 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 import { createStore } from "redux";
 
-const countReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-    };
-  }
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-    };
-  }
-  return state;
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const initialAuthState = {
+  isAuthenticated: false,
 };
 
-const store = createStore(countReducer);
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
 
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+});
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
