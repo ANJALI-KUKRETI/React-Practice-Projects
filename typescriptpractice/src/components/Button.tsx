14bcf3ac1bd415acbed173f213936fadea30fc96
Anjali <anjalikuku098@gmail.com>
2022-05-15 23:13:18 +0530

    ts react redux

--- typescriptpractice/src/components/Button.tsx
+++
@@ -1,8 +0,0 @@
-type ButtonProps = {
-  handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-};
-const Button = (props: ButtonProps) => {
-  return <button onClick={props.handleClick}>Click</button>;
-};
-
-export default Button;
